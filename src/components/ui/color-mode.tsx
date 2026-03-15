82f3dba3a4bb2c65e46e2de29263fdc2235e1d56
Colby <colby.b.cooper@gmail.com>
2026-03-15 17:31:29 -0400

    refactor: change interface to type for ColorModeProviderProps and ColorModeButtonProps

--- src/components/ui/color-mode.tsx
+++ src/components/ui/color-mode.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider, useTheme } from "next-themes";
 import * as React from "react";
 import { LuMoonStar, LuSun } from "react-icons/lu";
 
-export interface ColorModeProviderProps extends ThemeProviderProps {}
+export type ColorModeProviderProps = ThemeProviderProps;
 
 export function ColorModeProvider(props: ColorModeProviderProps) {
     return (
@@ -46,7 +46,7 @@ export function ColorModeIcon() {
     return colorMode === "dark" ? <LuMoonStar /> : <LuSun />;
 }
 
-interface ColorModeButtonProps extends Omit<IconButtonProps, "aria-label"> {}
+type ColorModeButtonProps = Omit<IconButtonProps, "aria-label">;
 
 export const ColorModeButton = React.forwardRef<
     HTMLButtonElement,
@@ -87,7 +87,7 @@ export const LightMode = React.forwardRef<HTMLSpanElement, SpanProps>(
                 {...props}
             />
         );
-    }
+    },
 );
 
 export const DarkMode = React.forwardRef<HTMLSpanElement, SpanProps>(
@@ -103,5 +103,5 @@ export const DarkMode = React.forwardRef<HTMLSpanElement, SpanProps>(
                 {...props}
             />
         );
-    }
+    },
 );
